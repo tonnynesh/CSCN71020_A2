016c172e0b500c102e3921064261ee2c1ec527df
Newton Nyabicha <nnyabicha2590@conestogac.on.ca>
2026-02-22 20:44:39 -0500

    Linked unit testing project to main project

---
+++ BCSRec_UnitTests/BCSRec_UnitTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../BCSRec/main.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BCSRecUnitTests
+{
+	TEST_CLASS(BCSRecUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
